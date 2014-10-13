ef4c6eeee0ab433ef84e1b6cc28189feb6bc7eb6
aerinzhang <shikunz@andrew.cmu.edu>
2014-10-12 22:58:30 -0700

    css

--- index_files/index.js
+++ index_files/index.js
@@ -1,9 +1,15 @@
 $( document ).ready(function(){
 	$('#aboutLink').click( 
 		function() {
-			$('#mainContent').html('Privafit是由派微科技有限公司推出的一款专注女性健身和社交的软件。\
-				Privafit致力于为广大年轻女性量身定制，以健康、美丽、自信为核心向导的社交分享平台。\
-				项目于2014年由美国硅谷和北京创立。创始人团队全部为来自美国著名高校和国内顶尖互联网公司的热爱生活的女生。'); 
+			$('#mainContent').html('\
+				<h4>我们的故事，Privafit的故事</h4>\
+				<p><br></p>\
+				<div class="innerContent">\
+					<p>Privafit是由派微科技有限公司推出的一款专注女性健身和社交的软件。</p>\
+					<p>Privafit致力于为广大年轻女性量身定制，以健康、美丽、自信为核心向导的社交分享平台。</p>\
+					<p>项目于2014年由美国硅谷和北京创立。</p>\
+					<p>创始人团队全部为来自美国著名高校和国内顶尖互联网公司的热爱生活的女生。</p>\
+				</div>'); 
 			return false;
 		} );
 	$('#functionLink').click(
@@ -22,19 +28,38 @@ $( document ).ready(function(){
 		});
 	$('#contactLink').click(
 		function() {
-			$('#mainContent').html('Have something brilliant to share with us? 发现我们有可以改进的地方？有更好的想法想和我们一起实现或由我们帮你实现？要想成为我们的一员并帮助其他妹子一起变美？任何你想说的，我们都愿意倾听！欢迎联络！');
+			$('#mainContent').html('\
+				<p>Have something brilliant to share with us?</p>\
+				<p>发现我们有可以改进的地方？</p>\
+				<p>有更好的想法想和我们一起实现或由我们帮你实现？</p>\
+				<p>要想成为我们的一员并帮助其他妹子一起变美？</p>\
+				<p><br></p>\
+				<p>任何你想说的，我们都愿意倾听！欢迎联络！</p>\
+				<div class="innerContent">\
+					<form >\
+							Name: <input type="text" name="name"><br>\
+							Email: <input type="text" name="email"><br>\
+							Subject: <input type="text" name="subject"><br>\
+							Message: <input type="text" name="message"><br>\
+							<input type="submit" value="Submit"><br>\
+					</form>\
+				</div>');
 			return false;
 		});
 
 	$('#homeLink').click(
 		function() {
-			$('#mainContent').html('在Privafit\
-				每个人都越来越美丽，越来越自信，越来越健康\
-				给自己一个机会，找到和你一起进步的伙伴！\
-				最不一样的美女app，你的私人健身闺蜜\
-				Trust us－\
-				We know what you want\
-				We build what you need');
+			$('#mainContent').html('\
+				<h4>在Privafit</h4>\
+				<p></br></p>\
+				<div class="innerContent">\
+					<p>每个人都越来越美丽，越来越自信，越来越健康</p>\
+					<p>给自己一个机会，找到和你一起进步的伙伴！</p>\
+					<p>最不一样的美女app，你的私人健身闺蜜</p>\
+					<p>Trust us－</p>\
+					<p>We know what you want</p>\
+					<p>We build what you need</p>\
+				</div>');
 			return false;
 		});
 })
